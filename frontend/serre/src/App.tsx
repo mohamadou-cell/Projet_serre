24699d153966fb6873acfea03545e517752163ca
MHDLamine <mouhamed.ngom50@gmail.com>
2023-03-18 18:15:34 +0000

    historique

--- frontend/serre/src/App.tsx
+++ frontend/serre/src/App.tsx
@@ -1,27 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Connexion from './composants/connexion'
-import Dashboard from './composants/dashboard'
-import Historique from './composants/historique'
-import Navbarre from './composants/navbarre'
-import Parametre from './composants/parametre'
-import Updatepassword from './composants/update_password'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Connexion from "./composants/connexion";
+import Dashboard from "./composants/dashboard";
+import Historique from "./composants/historique";
+import Navbarre from "./composants/navbarre";
+import Parametre from "./composants/parametre";
+import Updatepassword from "./composants/update_password";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    //Ceci c'est uniquement pour mieux commprendre le fonctionnement des coposants c'est à effacer apres 
+    //Ceci c'est uniquement pour mieux commprendre le fonctionnement des coposants c'est à effacer apres
     <div className="App">
-     <Connexion></Connexion>
-     <Dashboard></Dashboard>
-     <Historique></Historique>
-     <Navbarre></Navbarre>
-     <Parametre></Parametre>
-     <Updatepassword></Updatepassword>
+   
+      <Historique></Historique>
+    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

54827aa43c5ebc1f1d8cec1ed96f52625535feca
Richie MILANDOU <richierichard439@gmail.com>
2023-03-20 20:04:53 +0100

    recu

--- frontend/serre/src/composants/connexion.tsx
+++ frontend/serre/src/composants/connexion.tsx
@@ -1,7 +1,36 @@
-const Connexion =  () =>  {
- 
-    return (
-        <p>suis la page de connexion</p>
-    )}
+import { Card, Form } from "react-bootstrap"
+import Button from 'react-bootstrap/Button'
+import './styles/connexion.css'
+// import { React } from "react";
 
-export default Connexion;
+
+
+const Connexion = () => {
+
+return (
+<Card className="color-overlay d-flex
+justify-content-center align-items-center bg">
+<Form className="rounded p-4 p-sm-3" >
+<Form.Group className="mb-3"
+controlId="formBasicEmail">
+<Form.Label>Email</Form.Label>
+<Form.Control type="email" placeholder="veillez saisr votre email" />
+</Form.Group>
+
+<Form.Group className="mb-3" controlId="formBasicPassword">
+<Form.Label>Password</Form.Label>
+<Form.Control type="password" placeholder="veillez saisr votre password" />
+</Form.Group>
+<Form.Group className="mb-3" controlId="formBasicCheckbox">
+</Form.Group>
+
+<Button variant="primary" type="submit">
+Connexion
+</Button>
+
+</Form>
+</Card>
+);
+}
+
+export default Connexion

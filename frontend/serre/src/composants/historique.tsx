e36c092a3f5932c676939e61cc02d25f776db2f1
MHDLamine <mouhamed.ngom50@gmail.com>
2023-03-20 15:14:29 +0000

    finish

--- frontend/serre/src/composants/historique.tsx
+++ frontend/serre/src/composants/historique.tsx
@@ -3,15 +3,20 @@ import "./styles/historique.css";
 
 const Historique = () => {
   const [users, setUsers] = useState<any>(null);
+  const [start, setStart] = useState<number>(0);
+  const [end, setEnd] = useState<number>(7);
+
 
   useEffect(() => {
     fetch("http://localhost:5173/donnee.json")
       .then((res) => res.json())
       .then((res) => {
-        setUsers(res);
-        res;
+        
+        setUsers(res.filter((_:any, index: number)=> {
+          return index >= start && index < end
+        }));
       });
-  }, []);
+  }, [start, end]);
 
   const Data = () => {
   
@@ -34,7 +39,7 @@ const Historique = () => {
   return (
     <div className="box">
       <div className="h4-container">
-        <h4>Historique</h4>
+        <h4 className="h4_">Historique</h4>
       </div>
 
       <div className="table table_">
@@ -50,24 +55,24 @@ const Historique = () => {
         </table>
       </div>
       <nav aria-label="Page navigation example">
-        <ul className="pagination pagination_">
-          <li className="page-item">
-            <a className="page-link" href="#" aria-label="Previous">
+        <ul className="pagination pagination_ ">
+          <li className="page-item ">
+            <a className=" pagelinkupdate" href="#" aria-label="Previous" onClick={() => {setStart(0); setEnd(7)}}>
               <span aria-hidden="true">&laquo;</span>
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
+            <a className=" pagelinkupdate" href="#" onClick={() => {setStart(0); setEnd(7)}}>
               1
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#">
+            <a className=" pagelinkupdate" href="#" onClick={() => {setStart(7); setEnd(14)}}>
               2
             </a>
           </li>
           <li className="page-item">
-            <a className="page-link" href="#" aria-label="Next">
+            <a className=" pagelinkupdate" href="#" onClick={() => {setStart(7); setEnd(14)}} aria-label="Next">
               <span aria-hidden="true">&raquo;</span>
             </a>
           </li>

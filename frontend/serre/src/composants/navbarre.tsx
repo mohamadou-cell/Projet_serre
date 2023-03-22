fe6397252124e81cab8d7daf62230b5715af5457
mohamadou-cell <fallphilo91@gmail.com>
2023-03-22 16:30:01 +0000

    test

--- frontend/serre/src/composants/navbarre.tsx
+++ frontend/serre/src/composants/navbarre.tsx
@@ -15,7 +15,7 @@ function navbarre() {
                     <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                   </svg>
                     <ul className='sous'>
-                      <li className="items-sous-liste"><a className='a' href={`/modifmdp`}>Change password</a></li>
+                      <li className="items-sous-liste"><a className='a' href={`/modifmdp`}>Modifier mot de passe</a></li>
                       <li className='items-sous-liste'>
                       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-box-arrow-right fw-bold" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>

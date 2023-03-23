9545822b082e684a60769045bcc65ec35b88a2df
MHDLamine <mouhamed.ngom50@gmail.com>
2023-03-23 12:20:26 +0000

    finish

--- frontend/serre/src/composants/historique.tsx
+++ frontend/serre/src/composants/historique.tsx
@@ -2,38 +2,30 @@ import Navbarre from "./navbarre";
 import { useEffect, useState } from "react";
 import "./styles/historique.css";
 
-
-
 const Historique = () => {
   const [users, setUsers] = useState<any>(null);
   const [start, setStart] = useState<number>(0);
-  const [active1, setActive1] = useState<boolean>(true)
-  const [active2, setActive2] = useState<boolean>(false)
+  const [active1, setActive1] = useState<boolean>(true);
+  const [active2, setActive2] = useState<boolean>(false);
   const [end, setEnd] = useState<number>(7);
-
 
   useEffect(() => {
     fetch("http://localhost:5173/donnee.json")
       .then((res) => res.json())
       .then((res) => {
-        
-        setUsers(res.filter((_:any, index: number)=> {
-          return index >= start && index < end
-        }));
+        setUsers(
+          res.filter((_: any, index: number) => {
+            return index >= start && index < end;
+          })
+        );
       });
   }, [start, end]);
 
-
-
-
   const Data = () => {
-  
-    
     return (
       <tbody>
         {users?.map((user: any) => (
           <tr>
-            
             <td className="td_">{user.Date}</td>
             <td className="td_">{user.temp}</td>
             <td className="td_">{user.humi_sol}</td>
@@ -46,51 +38,93 @@ const Historique = () => {
   };
 
   return (
-    <><Navbarre></Navbarre>
-   
-    <div className="container box">
-     
-      <div className="h4-container">
-        <h4 className="h4_">Historique</h4>
-      </div>
+    <>
+      <Navbarre></Navbarre>
 
-      <div className="table table_">
-        <table border={1}>
-          <thead className="backblue">
-            <td className="td_">Date</td>
-            <td className="td_">Temperature en °C</td>
-            <td className="td_">Humidité sol en %</td>
-            <td className="td_">Humidité serre en %</td>
-            <td className="td_">luminosité en lux</td>
-          </thead>
-          <Data></Data>
-        </table>
+      <div className="container box">
+        <div className="h4-container">
+          <h4 className="h4_">Historique</h4>
+        </div>
+
+        <div className="table table_">
+          <table border={1}>
+            <thead className="backblue">
+              <td className="td_">Date</td>
+              <td className="td_">Temperature en °C</td>
+              <td className="td_">Humidité sol en %</td>
+              <td className="td_">Humidité serre en %</td>
+              <td className="td_">luminosité en lux</td>
+            </thead>
+            <Data></Data>
+          </table>
+        </div>
+        <nav aria-label="Page navigation example">
+          <ul className="pagination pagination_ ">
+            <li className="page-item ">
+              <a
+                className=" pagenav"
+                href="#"
+                aria-label="Previous"
+                onClick={() => {
+                  setStart(0);
+                  setEnd(7);
+                  setActive1(true);
+                  setActive2(false);
+                }}
+              >
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className={`pagelinkupdate ${active1 ? "bg-focus" : ""}`}
+                id="un"
+                href="#"
+                onClick={() => {
+                  setStart(0);
+                  setEnd(7);
+                  setActive1(true);
+                  setActive2(false);
+                  console.log(document.getElementById('un'));
+                }}
+              >
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className={`pagelinkupdate ${active2 ? "bg-focus" : ""}`}
+                href="#"
+                onClick={() => {
+                  setStart(7);
+                  setEnd(14);
+                  setActive1(false);
+                  setActive2(true);
+                  console.log(document.getElementById('un'));
+                }}
+              >
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className=" pagenav"
+                href="#"
+                onClick={() => {
+                  setStart(7);
+                  setEnd(14);
+                  setActive1(false);
+                  setActive2(true);
+                }}
+                aria-label="Next"
+              >
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav aria-label="Page navigation example">
-        <ul className="pagination pagination_ ">
-          <li className="page-item ">
-            <a className=" pagenav" href="#" aria-label="Previous" onClick={() => { setStart(0); setEnd(7); setActive1(true); setActive2(false);  } }>
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li className="page-item">
-            <a className={`pagelinkupdate ${active1 ? "bg-focus": ""}`} id="un" href="#" onClick={() => { setStart(0); setEnd(7); setActive1(true); setActive2(false);} }>
-              1
-            </a>
-          </li>
-          <li className="page-item">
-            <a className={`pagelinkupdate ${active2 ? "bg-focus": ""}`} href="#" onClick={() => { setStart(7); setEnd(14); setActive1(false); setActive2(true); } }>
-              2
-            </a>
-          </li>
-          <li className="page-item">
-            <a className=" pagenav" href="#" onClick={() => {  setStart(7); setEnd(14); setActive1(false); setActive2(true); } } aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div></>
+    </>
   );
 };
 

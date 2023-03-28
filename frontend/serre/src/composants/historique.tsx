6e89b57542a79545ef2a333022c07cee57261a19
MHDLamine <mouhamed.ngom50@gmail.com>
2023-03-28 09:14:16 +0000

    last push in frontend'

--- frontend/serre/src/composants/historique.tsx
+++ frontend/serre/src/composants/historique.tsx
@@ -10,7 +10,12 @@ const Historique = () => {
   const [active2, setActive2] = useState<boolean>(false);
   const [rechercher, setRecherche] = useState<String>("");
   const [cacher2, setCacher2] = useState<boolean>(true);
+  const jour = new Date().getDate();
+  const mois = new Date().getMonth() + 1;
+  const annee = new Date().getFullYear();
+  const max = `${annee}-0${mois}-${jour}`
 
+  
 
 
   useEffect(() => {
@@ -18,15 +23,13 @@ const Historique = () => {
       .then((res) => res.json())
       .then((res) => {
         //masquer ou afficher la pagination
-          if (res.length > 7) {
-              setCacher2(false);
-               } 
+        if (res.length > 7) {
+          setCacher2(false);
+        } else {
+          setCacher2(true);
+        }
         setUsers(
-     
           res.filter((_a: any, index: number) => {
-           
-             
-               
             if (rechercher == "") {
               return index >= start && index < end;
             } else {
@@ -40,7 +43,7 @@ const Historique = () => {
   const fleche = () => {
     if (active1 == true) {
       setActive1(false);
-      setActive2(true)
+      setActive2(true);
       setStart(7);
       setEnd(14);
     }
@@ -50,7 +53,7 @@ const Historique = () => {
       setStart(0);
       setEnd(7);
     }
-  }
+  };
 
   const Data = () => {
     return (
@@ -67,14 +70,11 @@ const Historique = () => {
       </tbody>
     );
   };
-  const search = (chercher:any) => {
+  const search = (chercher: any) => {
     const valeur = chercher;
-  
-  
+
     setRecherche(valeur);
-    const jour = new Date().getDate();
-    const mois = new Date().getMonth() + 1;
-    const annee = new Date().getFullYear();
+    
   };
 
   return (
@@ -87,85 +87,87 @@ const Historique = () => {
         </div>
 
         <div className="table_">
-        <div className="table">
-          <input onChange={(e) => search(e.target.value)} type="date" name="date" id="date" />
-          <table border={1}>
-            <thead className="backblue">
-              <td className="td_ th_"> Date </td>
-              <td className="td_ th_" >Temperature en °C</td>
-              <td className="td_ th_">Humidité sol en %</td>
-              <td className="td_ th_">Humidité serre en %</td>
-              <td className="td_ th_">luminosité en lux</td>
-            </thead>
-            <Data></Data>
-          </table>
-          <div className="box-pagination">
-            <nav aria-label="Page navigation example">
-              <ul className="pagination pagination_ ">
-                <li className="page-item ">
-                  <a
-                    className=" pagenav"
-                    href="#"
-                    aria-label="Previous"
-                    onClick={() => {
-                      /* setStart(0);
-                      setEnd(7);
-                      setActive1(true);
-                      setActive2(false); */
-                      fleche();
-                    }}
-                  >
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a
-                    className={`pagelinkupdate ${active1 ? "bg-focus" : ""} `}
-                    id="un"
-                    href="#"
-                    onClick={() => {
-                      setStart(0);
-                      setEnd(7);
-                      setActive1(true);
-                      setActive2(false);
-                    }}
-                  >
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a
-                    className={`pagelinkupdate ${active2 ? "bg-focus" : ""} ${cacher2 ? "cacher" : ""}`}
-                    href="#"
-                    onClick={() => {
-                      setStart(7);
-                      setEnd(14);
-                      setActive1(false);
-                      setActive2(true);
-                    }}
-                  >
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a
-                    className=" pagenav"
-                    href="#"
-                    onClick={() => {
-                    /*   setStart(7);
-                      setEnd(14);
-                      setActive1(false);
-                      setActive2(true); */
-                      fleche();
-                    }}
-                    aria-label="Next"
-                  >
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <div className="table">
+            <input
+              onChange={(e) => search(e.target.value)}
+              type="date"
+              name="date"
+              id="date"
+              max={max}
+            />
+            <table border={1}>
+              <thead className="backblue">
+                <td className="td_ th_"> Date </td>
+                <td className="td_ th_">Temperature en °C</td>
+                <td className="td_ th_">Humidité sol en %</td>
+                <td className="td_ th_">Humidité serre en %</td>
+                <td className="td_ th_">luminosité en lux</td>
+              </thead>
+              <Data></Data>
+            </table>
+            <div className="box-pagination">
+              <nav aria-label="Page navigation example">
+                <ul className="pagination pagination_ ">
+                  <li className="page-item ">
+                    <a
+                      className={`pagenav ${cacher2 ? "cacher" : ""}`}
+                      href="#"
+                      aria-label="Previous"
+                      onClick={() => {
+                        fleche();
+                      }}
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className={`pagelinkupdate ${
+                        active1 ? "bg-focus" : ""
+                      }  `}
+                      id="un"
+                      href="#"
+                      onClick={() => {
+                        setStart(0);
+                        setEnd(7);
+                        setActive1(true);
+                        setActive2(false);
+                      }}
+                    >
+                      1
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className={`pagelinkupdate ${active2 ? "bg-focus" : ""} ${
+                        cacher2 ? "cacher" : ""
+                      }`}
+                      href="#"
+                      onClick={() => {
+                        setStart(7);
+                        setEnd(14);
+                        setActive1(false);
+                        setActive2(true);
+                      }}
+                    >
+                      2
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a
+                      className={`pagenav ${cacher2 ? "cacher" : ""}`}
+                      href="#"
+                      onClick={() => {
+                        fleche();
+                      }}
+                      aria-label="Next"
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>

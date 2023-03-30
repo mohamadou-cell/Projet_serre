23c1aa136e26c12ce2f42e6a619ca62032c9c278
MHDLamine <mouhamed.ngom50@gmail.com>
2023-03-30 16:40:44 +0000

    token

--- frontend/serre/src/composants/navbarre.tsx
+++ frontend/serre/src/composants/navbarre.tsx
@@ -1,16 +1,26 @@
+import { getItem } from "localforage";
 import { useEffect, useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import './styles/navebarre.css'
 function navbarre() {
-  const [infos, setInfos] = useState<any>(null)
+  const [infos, removeInfos] = useState<any>(null)
+
   useEffect(() => {
     fetch("http://localhost:5173/user.json")
       .then((res) => res.json())
       .then((res) => {
-        setInfos(res);
+        removeInfos(res);
         console.log(res);
       });
   }, []);
+
+  const logout = () =>{
+    localStorage.removeItem("email" );
+    localStorage.removeItem("prenom");
+    localStorage.removeItem("nom");
+    localStorage.removeItem("tokoen");
+  }
+
   return (
     <div > 
       <nav className="navbar" id='navbar'>
@@ -18,12 +28,16 @@ function navbarre() {
           <img  src="../public/user4.jpg" className='img' alt="" /> 
           <div className='drop'> 
             <div className='d-flex gap-2'>
-              {infos?.map((info:any)=>(
+           {/*    {infos?.map((info:any)=>(
                 <p className='info'>{info.prenom}</p>
+                <p className='info'>{prenom}</p>
               ))}
               {infos?.map((info:any)=>(
                 <p className='info'>{info.nom}</p>
-              ))}
+                <p className='info'>{nom}</p>
+              ))} */}
+                  <p className='info'>{localStorage.getItem('prenom')}</p>
+                  <p className='info'>{localStorage.getItem('nom')}</p>
               </div>
               <div className="dropdown d-flex justify-content-center">
                 <li className='items'>
@@ -37,7 +51,7 @@ function navbarre() {
                         <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
                         <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                       </svg>
-                        <a className='a' href={`/connection`}>Deconnection</a>
+                        <a className='a' onClick={()=>{logout()}} href={`/connection`}>Deconnection</a>
                         </li>
                     </ul>
                   </li>

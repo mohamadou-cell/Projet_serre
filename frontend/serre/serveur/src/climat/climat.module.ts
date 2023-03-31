a18ca0c5f946cce19afca9e10363f1193b12e74f
gonarhane <gonar90@gmail.com>
2023-03-31 14:58:07 +0000

    récuprer les donnees

--- frontend/serre/serveur/src/climat/climat.module.ts
+++ frontend/serre/serveur/src/climat/climat.module.ts
@@ -1,12 +1,29 @@
-import { Module } from '@nestjs/common';
+/* import { Module } from '@nestjs/common';
 import { ClimatService } from './climat.service';
 import { ClimatController } from './climat.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClimatSchema } from './entities/climat.entity';
+import { ClimatGateway } from './climat.gateway';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Climat', schema: ClimatSchema }])],
   controllers: [ClimatController],
-  providers: [ClimatService]
+  providers: [ClimatService, ClimatGateway]
+})
+export class ClimatModule {} */
+import { Module } from '@nestjs/common';
+import { ClimatGateway } from './climat.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Climat, ClimatSchema } from './entities/climat.entity';
+import { ClimatService } from './climat.service';
+import { ClimatController } from './climat.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Climat.name, schema: ClimatSchema }]),
+  ],
+  providers: [ClimatGateway, ClimatService],
+  controllers: [ClimatController],
+  exports: [ClimatService],
 })
 export class ClimatModule {}
